4dad9719755e40cf0545dc6e12bb82fe93df4e10
golazin <abolfathig@student.douglascollege.ca>
2021-03-10 08:38:10 -0800

    initializing the project

---
+++ battleship-v7/src/main/java/com/example/battleshipv7/BattleshipV7Application.java
@@ -0,0 +1,13 @@
+package com.example.battleshipv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipV7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipV7Application.class, args);
+	}
+
+}
---
+++ battleship-v7/src/test/java/com/example/battleshipv7/BattleshipV7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battleshipv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipV7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
